8abd18069fb53e2e8a523d879204480ab24dbf25
Jason R Alexander <sunnysidesounds@gmail.com>
2020-08-02 22:40:23 -0700

    More updates

---
+++ scripts/set_environment_variables.sh
@@ -0,0 +1,6 @@
+# Set Environment Variables for DB
+export SECRET_KEY='this-is-a-secret'
+export MYSQL_USER='test'
+export MYSQL_PASSWORD='test'
+export MYSQL_HOST='localhost'
+export MYSQL_DATABASE='testdb'
